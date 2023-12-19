d83dbba278c4dd77000a348f518fe6bdaad564a7
onurgemici <146668665+onurgemici@users.noreply.github.com>
2023-12-19 21:14:39 +0300

    database added

--- TeknoMarket/Program.cs
+++ TeknoMarket/Program.cs
@@ -1,7 +1,38 @@
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using TeknoMarketData;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+
+builder
+    .Services
+    .AddDbContext<AppDbContext>(config =>
+    {
+        var dbProvider = builder.Configuration.GetValue<string>("DbProvider");
+        var cs = builder.Configuration.GetConnectionString(dbProvider!);
+        switch (dbProvider)
+        {
+            case "PostgreSql":
+                config.UseNpgsql(cs, p => p.MigrationsAssembly("MigrationsPostgreSql"));
+                break;
+            case "MySql":
+                config.UseMySql(cs, ServerVersion.AutoDetect(cs), p => p.MigrationsAssembly("MigrationsMySql"));
+                break;
+            case "SqlServer":
+            default:
+                config.UseSqlServer(cs, p => p.MigrationsAssembly("MigrationsSqlServer"));
+                break;
+        }
+    });
 
 var app = builder.Build();
 
